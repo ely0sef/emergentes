e3534406e764b3f7dde29edd90d4900aab1865cf
ely0sef <a19310162@live.ceti.mx>
2023-06-14 00:34:51 -0600

    inter

--- app/src/main/java/com/example/rosa/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/rosa/ui/notifications/NotificationsFragment.java
@@ -1,37 +1,77 @@
 package com.example.rosa.ui.notifications;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.rosa.databinding.FragmentNotificationsBinding;
+import com.example.rosa.R;
+import com.microsoft.azure.sdk.iot.device.DeviceClient;
+import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeCallback;
+import com.microsoft.azure.sdk.iot.device.IotHubConnectionStatusChangeReason;
+import com.microsoft.azure.sdk.iot.device.IotHubMessageResult;
+import com.microsoft.azure.sdk.iot.device.Message;
+import com.microsoft.azure.sdk.iot.device.MessageCallback;
+import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 
 public class NotificationsFragment extends Fragment {
+    private static final String CONNECTION_STRING = "HostName=rbpecera.azure-devices.net;DeviceId=picow;SharedAccessKey=ox92G0pJ9Nju26+psgelqWfxtEOqpF0LN/SlKH7bNoM=";
+    private static final String DEVICE_ID = "android-device";
 
-    private FragmentNotificationsBinding binding;
+    private DeviceClient deviceClient;
+    private EditText txtTemp;
+    private DeviceClient client;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+    private Handler handler;
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        txtTemp = root.findViewById(R.id.edit_max_temperature);
 
-        //final TextView textView = binding.textNotifications;
-       // notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        handler = new Handler(Looper.getMainLooper());
+        connectToIoTHub();
+
         return root;
     }
+    private void connectToIoTHub() {
+        try {
+            deviceClient = new DeviceClient(CONNECTION_STRING, IotHubClientProtocol.MQTT);
+            deviceClient.registerConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallback() {
+                @Override
+                public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason reason, Throwable throwable, Object callbackContext) {
+                    // Manejar cambios de estado de conexión
+                }
+            }, null);
 
+            deviceClient.open();
+
+            deviceClient.setMessageCallback(new MessageCallback() {
+                @Override
+                public IotHubMessageResult execute(Message message, Object callbackContext) {
+                    // Se recibió un mensaje de IoT Hub
+                    String payload = new String(message.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
+                    Log.d("AzureIoT", "Mensaje recibido: " + payload);
+                    txtTemp.setText(payload);
+                    // Realizar las acciones necesarias con el mensaje recibido
+
+                    return IotHubMessageResult.COMPLETE;
+                }
+            }, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+
     }
 }
