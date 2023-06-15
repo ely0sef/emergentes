e6fe6ee88d06212b40af281736df6e7e387f65e1
ely0sef <a19310162@live.ceti.mx>
2023-06-14 21:18:21 -0600

    infomessages

--- app/src/main/java/com/example/rosa/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/rosa/ui/notifications/NotificationsFragment.java
@@ -22,56 +22,77 @@ import com.microsoft.azure.sdk.iot.device.Message;
 import com.microsoft.azure.sdk.iot.device.MessageCallback;
 import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 
+import java.io.IOException;
+
 public class NotificationsFragment extends Fragment {
     private static final String CONNECTION_STRING = "HostName=rbpecera.azure-devices.net;DeviceId=picow;SharedAccessKey=ox92G0pJ9Nju26+psgelqWfxtEOqpF0LN/SlKH7bNoM=";
-    private static final String DEVICE_ID = "android-device";
+    private static final String DEVICE_ID = "picow";
 
     private DeviceClient deviceClient;
     private EditText txtTemp;
-    private DeviceClient client;
 
     private Handler handler;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         txtTemp = root.findViewById(R.id.edit_max_temperature);
-
         handler = new Handler(Looper.getMainLooper());
-        connectToIoTHub();
+
+        connectToDevice();
 
         return root;
     }
-    private void connectToIoTHub() {
+
+    private void connectToDevice() {
         try {
             deviceClient = new DeviceClient(CONNECTION_STRING, IotHubClientProtocol.MQTT);
             deviceClient.registerConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallback() {
-                @Override
                 public void execute(IotHubConnectionStatus status, IotHubConnectionStatusChangeReason reason, Throwable throwable, Object callbackContext) {
-                    // Manejar cambios de estado de conexión
+                    if (status == IotHubConnectionStatus.DISCONNECTED) {
+                        Log.e("DeviceClient", "Desconectado. Se intentará reconectar...");
+                        try {
+                            deviceClient.open();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
             }, null);
 
             deviceClient.open();
 
-            deviceClient.setMessageCallback(new MessageCallback() {
-                @Override
-                public IotHubMessageResult execute(Message message, Object callbackContext) {
-                    // Se recibió un mensaje de IoT Hub
-                    String payload = new String(message.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
-                    Log.d("AzureIoT", "Mensaje recibido: " + payload);
-                    txtTemp.setText(payload);
-                    // Realizar las acciones necesarias con el mensaje recibido
+            deviceClient.setMessageCallback(new MessageCallback(), null);
 
-                    return IotHubMessageResult.COMPLETE;
-                }
-            }, null);
+            Log.i("DeviceClient", "Conexión establecida. Esperando mensajes...");
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e("DeviceClient", "Error al conectarse al dispositivo: " + e.getMessage());
         }
     }
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (deviceClient != null) {
+            try {
+                deviceClient.closeNow();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private class MessageCallback implements com.microsoft.azure.sdk.iot.device.MessageCallback {
+        public IotHubMessageResult execute(Message msg, Object context) {
+            final String messageData = new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
+
+            handler.post(new Runnable() {
+                public void run() {
+                    Log.i("DeviceClient", "Mensaje recibido: " + messageData);
+                    txtTemp.setText(messageData);
+                }
+            });
+
+            return IotHubMessageResult.COMPLETE;
+        }
     }
 }
